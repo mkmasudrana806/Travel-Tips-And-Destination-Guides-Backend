c4f8023eaa21d748677ccbee39518b3cfb2c7a90
mkmasudrana806 <mkmasudrana806@gmail.com>
2026-02-27 10:05:09 +0600

    refactor: Added viewerContext metadata in response for personalized response

--- src/app/utils/sendResponse.ts
+++ src/app/utils/sendResponse.ts
@@ -1,23 +1,25 @@
 import { Response } from "express";
 
-type TSendResponse<T, U> = {
+type TSendResponse<T, U, V> = {
   statusCode: number;
   success: boolean;
   message: string;
   meta?: U;
   data: T;
+  viewerContext?: V;
 };
 
 /**
  * @param res res  object
  * @param data data
  */
-const sendResponse = <T, U>(res: Response, data: TSendResponse<T, U>) => {
+const sendResponse = <T, U, V>(res: Response, data: TSendResponse<T, U, V>) => {
   res.status(data.statusCode).json({
     success: data.success,
     message: data.message,
     meta: data?.meta,
     data: data.data,
+    viewerContext: data?.viewerContext,
   });
 };
 
